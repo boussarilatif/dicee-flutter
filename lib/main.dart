c85acfc4189b373e3ae341d12ed4e377a68c5610
Boussari Latif <latif.boussari@mehriso.com>
2019-09-10 21:31:24 +0100

    add interaction in your dice app

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/rendering.dart';
+import "dart:math";
 
 void main() {
   // debugPaintSizeEnabled=true;
@@ -18,27 +19,47 @@ void main() {
   ));
 }
 
-class DicePage extends StatelessWidget {
+class DicePage extends StatefulWidget {
+  @override
+  _DicePageState createState() => _DicePageState();
+}
+
+class _DicePageState extends State<DicePage> {
+  int leftDiceNumber = 1;
+  int rightDiceNumber = 1;
+  final  list = [1,2,3,4,5];
+  final _random = new Random();
   @override
   Widget build(BuildContext context) {
-    var leftDice = 5;
     return Center(
       child: Row(
         children: <Widget>[
           Expanded(
             child: FlatButton(
               child: Image(
-                image: AssetImage('images/dice$leftDice.png'),
+                image: AssetImage('images/dice$leftDiceNumber.png'),
                 width: 200.0,
               ),
+              onPressed: (){
+                setState(() {
+                  leftDiceNumber = list[_random.nextInt(list.length)];
+                  rightDiceNumber = list[_random.nextInt(list.length)];
+                });
+              }, 
             ),
           ),
           Expanded(
             child: FlatButton(
               child: Image(
-                image: AssetImage('images/dice2.png'),
+                image: AssetImage('images/dice$rightDiceNumber.png'),
                 width: 200.0,
               ),
+              onPressed: (){
+                setState(() {
+                  leftDiceNumber = list[_random.nextInt(list.length)];
+                  rightDiceNumber = list[_random.nextInt(list.length)];
+                });
+              },
             ),
           )
         ],
